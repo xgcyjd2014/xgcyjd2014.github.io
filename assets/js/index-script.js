ba82a022e1293a605139cfe4232aa4072000111c
NumerHero <469564715@qq.com>
2015-11-30 15:10:19 +0800

    new version

--- assets/js/index-script.js
+++ assets/js/index-script.js
@@ -1,14 +1,14 @@
-						/*--------------------------------------------*\
-
-						   		   Design And Build By Owen
-						      Github:http://github.com/numerhero
-
-						\*--------------------------------------------*/
+							
+							/*--------------------------------------------*\
+									  信管创业基地官网 宣传页js
+							   		   Design And Build By Owen
+							      Github:http://github.com/numerhero
+							\*--------------------------------------------*/
 
 window.console && console.log && console.log("%c          ___________             ___     ___\n         /   /       \\           /  /__  /  /__\n        /   /         \\         /_____/ /_____/\n       /    |__________\\       |    _______    |\n      /     |_________|        |_  |_______|  _|\n     /  /|  | | |___| |            |__________\n    /__/ |__| |_______|            |__________| \n \n        __    __                               __\n    ___|  |__|  |___            ___       __  |  | \n   |____   __    ___|          |   |     |  |_|  |__ \n  ___|____|_ _|____|___      __|   |__   |   _    __|\n |_____________________|    |___   ___|  |  | |  |\n   /  ____|___|____  \\         |   |     |  | |__|    _\n /___/  __|   |__  \\___\\   ____|   |__   |  |________| |\n      |___________|       |___________|  |_____________|","color:transparent;text-shadow:0 0 1px rgb(240,107,134);");
 window.console && console.log && console.log("\n      欢迎加入信管创业基地 %c 面试时请附上console","font:15px/20px '微软雅黑';  background-image: -webkit-linear-gradient(left, #147B96, #E6D205 25%, #147B96 50%, #E6D205 75%, #147B96);-webkit-text-fill-color: transparent;-webkit-background-clip: text; color:red;");
 
-// 开场动画
+// 开场动画(愿景页)
 $(document).ready(function(){
 	var oImg  = $(".page1-content img");
 	var font1 = $(".page1-content-font1");
@@ -205,12 +205,84 @@ $(function(){
 		})
 		oSlideA.eq(iNow).addClass("pagecount-active");
 	}
+});
 
+// 报名页
+$(function(){
+	var moveElements = $(".enroll-show-elements");
+	var X = 800;
+	var Z = 400;
+	var EnrollInow = 0;
+
+	var enrollShowListArrow = $(".enroll-show-list-arrow");
+
+	enrollShowListArrow.eq(0).on("click" , function(){
+		if(EnrollInow == 0)
+		{
+			return;
+		}
+
+		moveElements.eq(EnrollInow).css("transform" , "translateX("+X+"px) translateZ("+Z+"px) rotateY(-80deg)");
+		moveElements.eq(EnrollInow).css("webkitTransform" , "translateX("+X+"px) translateZ("+Z+"px) rotateY(-80deg)");
+		
+		moveElements.eq(EnrollInow).css("opacity" , "0");
+		for(var i = 0; i<EnrollInow; i++)
+		{
+			moveElements.eq(i).css("transform" , "translateX(-"+(EnrollInow-i-1)*X+"px) translateZ("+Z+"px) rotateY(80deg)");
+			moveElements.eq(i).css("webkitTransform" , "translateX(-"+(EnrollInow-i-1)*X+"px) translateZ("+Z+"px) rotateY(80deg)");
+		}
+		
+
+
+		EnrollInow--;
+
+		moveElements.eq(EnrollInow).css("transform" , "translateX(0) translateZ(10px) rotateY(0)");	
+		moveElements.eq(EnrollInow).css("webkitTransform" , "translateX(0) translateZ(10px) rotateY(0)");	
+		
+		moveElements.eq(EnrollInow).css("opacity" , "1");
+
+		for(var i =  moveElements.length - 1 ; i>EnrollInow ; i--)
+		{
+			moveElements.eq(i).css("transform" , "translateX("+ (i - EnrollInow) *X +"px) translateZ("+Z+"px) rotateY(-80deg)")
+			moveElements.eq(i).css("webkitTransform" , "translateX("+ (i - EnrollInow) *X +"px) translateZ("+Z+"px) rotateY(-80deg)")
+		}
+	});
+
+	enrollShowListArrow.eq(1).on("click" , function(){
+		if(EnrollInow == moveElements.length - 1)
+		{
+			return;
+		}
+
+		for(var i = 0; i<EnrollInow; i++)
+		{
+			moveElements.eq(i).css("transform" , "translateX(-"+(EnrollInow + 1 -i)*X+"px) translateZ("+Z+"px) rotateY(80deg)");
+			moveElements.eq(i).css("webkitTransform" , "translateX(-"+(EnrollInow + 1 -i)*X+"px) translateZ("+Z+"px) rotateY(80deg)");
+		}
+		moveElements.eq(EnrollInow).css("transform" , "translateX(-"+X+"px) translateZ("+Z+"px) rotateY(80deg)");
+		moveElements.eq(EnrollInow).css("webkitTransform" , "translateX(-"+X+"px) translateZ("+Z+"px) rotateY(80deg)");
+		
+		moveElements.eq(EnrollInow).css("opacity" , "0");
+
+		EnrollInow++;
+
+		moveElements.eq(EnrollInow).css("transform" , "translateX(0) translateZ(10px) rotateY(0)");	
+		moveElements.eq(EnrollInow).css("webkitTransform" , "translateX(0) translateZ(10px) rotateY(0)");	
+		
+		moveElements.eq(EnrollInow).css("opacity" , "1");
+
+		for(var i =  moveElements.length - 1 ; i>EnrollInow ; i--)
+		{
+			moveElements.eq(i).css("transform" , "translateX("+ (i - EnrollInow) *X +"px) translateZ("+Z+"px) rotateY(-80deg)")
+			moveElements.eq(i).css("webkitTransform" , "translateX("+ (i - EnrollInow) *X +"px) translateZ("+Z+"px) rotateY(-80deg)")
+		
+		}
+	});
 
 });
 
 
-
+// 禁止用户缩放视窗
 function scrollFunc(ev){ 
 	ev = ev || window.event; 
 	if(ev.wheelDelta && ev.ctrlKey) //IE/Opera/Chrome 
