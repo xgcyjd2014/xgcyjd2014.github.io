ca0824522d9639e382b8cb7fe99ab6128a867d1f
hi-craft <363511503@qq.com>
2018-12-02 17:23:33 +0800

    ...

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,43 +1,63 @@
-							
 							/*--------------------------------------------*\
-									  	  信管创业基地 main.js
-							   		   Design And Build By 倪云港
-							   		   Email: yungangni@gmail.com
-							     Github: http://github.com/niyungang
-							\*--------------------------------------------*/
+																																																			  	  信管创业基地 main.js
+																																																	   		   Design And Build By 倪云港
+																																																	   		   Email: yungangni@gmail.com
+																																																	     Github: http://github.com/niyungang
+																																																	\*--------------------------------------------*/
 
-//鼠标点击特效
-function beautiful(){
-	var html = document.getElementsByTagName("html")[0];
-	var body = document.getElementsByTagName("body")[0];
+							//鼠标点击特效
+							function beautiful() {
+							    var html = document.getElementsByTagName("html")[0];
+							    var body = document.getElementsByTagName("body")[0];
 
-	html.onclick = function(e){
-		var b = document.createElement("b");
-		b.innerHTML = "❤";
-		body.append( b );
+							    html.onclick = function(e) {
+							        var b = document.createElement("b");
+							        b.innerHTML = "❤";
+							        body.append(b);
 
-		var x = e.pageX - 5;
-		var y = e.pageY - 20;
+							        var x = e.pageX - 5;
+							        var y = e.pageY - 20;
 
-		var n = Math.floor(Math.random() * 14 + 8);
+							        var n = Math.floor(Math.random() * 14 + 8);
 
-		b.style.zIndex = 2;
-		b.style.position = "absolute";
-		b.style.left = x + "px";
-		b.style.top = y + "px";
-		b.style.color = "red";
-		b.style.fontSize = n + "px";
-		b.style.userSelect = "none";
+							        b.style.zIndex = 2;
+							        b.style.position = "absolute";
+							        b.style.left = x + "px";
+							        b.style.top = y + "px";
+							        b.style.color = "red";
+							        b.style.fontSize = n + "px";
+							        b.style.userSelect = "none";
 
-		setTimeout(function(){
-			b.style.top = (y - 160) + "px";
-			b.style.opacity = "0";
-			b.style.transition = "1.6s";
-		},1)
-	}
-}
-beautiful();
+							        setTimeout(function() {
+							            b.style.top = (y - 160) + "px";
+							            b.style.opacity = "0";
+							            b.style.transition = "1.6s";
+							        }, 1)
+							    }
+							}
+							beautiful();
 
 
-// console
-window.console && console.log && console.log("\n %c 黑大信管创业基地 \n","font:15px/20px '微软雅黑';  background-image: -webkit-linear-gradient(left, #147B96, #E6D205 25%, #147B96 50%, #E6D205 75%, #147B96);-webkit-text-fill-color: transparent;-webkit-background-clip: text; color:red;");
+							// console
+							window.console && console.log && console.log([
+							    "                   _ooOoo_",
+							    "                  o8888888o",
+							    "                  88\" . \"88",
+							    "                  (| -_- |)",
+							    "                  O\\  =  /O",
+							    "               ____/`---'\\____",
+							    "             .'  \\\\|     |//  `.",
+							    "            /  \\\\|||  :  |||//  \\",
+							    "           /  _||||| -:- |||||-  \\",
+							    "           |   | \\\\\\  -  /// |   |",
+							    "           | \\_|  ''\\---/''  |   |",
+							    "           \\  .-\\__  `-`  ___/-. /",
+							    "         ___`. .'  /--.--\\  `. . __",
+							    "      .\"\" '<  `.___\\_<|>_/___.'  >'\"\".",
+							    "     | | :  `- \\`.;`\\ _ /`;.`/ - ` : | |",
+							    "     \\  \\ `-.   \\_ __\\ /__ _/   .-` /  /",
+							    "======`-.____`-.___\\_____/___.-`____.-'======",
+							    "                   `=---='",
+							    "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^",
+							    "         佛祖保佑       永无BUG"
+							].join('\n'));
